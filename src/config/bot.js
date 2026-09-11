4b967b92b27b934dfc4d82320b1514d1b99bd353
naji4mayor <najidillingham2009@gmail.com>
2026-09-11 13:33:22 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#7393B3",
+      secondary: "#5F9EA0",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
