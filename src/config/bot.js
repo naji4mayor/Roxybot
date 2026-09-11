2951b3c8cbf4a52548ca9fa5df3c81a66610dbd1
naji4mayor <najidillingham2009@gmail.com>
2026-09-11 13:29:58 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Dating Simulator 2", // required by Discord API, not shown in the client
-        state: "",     // this is what people actually see
+        state: "Dating Simulator 2",     // this is what people actually see
         type: 0,               // Dating simulator
       },
     ],
